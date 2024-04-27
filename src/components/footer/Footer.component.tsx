92bd826a57ffcc2dc6a2e16b63a85ff49ee4899a
demasha0908 <demchenko.mariia0908@gmail.com>
2024-04-28 01:18:41 +0300

    done task 02-interfaces

---
+++ src/components/footer/Footer.component.tsx
@@ -0,0 +1,36 @@
+import Facebook from '@/assets/fb.png';
+import Instagram from '@/assets/insta.png';
+import Linkedin from '@/assets/linkedin.png';
+
+import styles from './footer.module.css';
+
+export const FooterComponent = () => (
+    <footer className={styles.footer}>
+        <div className={styles.footer__content}>
+            <ul className={styles.footer__links}>
+                <li>
+                    <a className={styles.footer__link} href={'https://uk-ua.facebook.com/'} target="_blank" rel="noreferrer">
+                        <img className={styles.linkicon} src={Facebook} alt="Facebook" />
+                    </a>
+                </li>
+                <li>
+                    <a className={styles.footer__link} href={'https://www.linkedin.com/'} target="_blank" rel="noreferrer">
+                        <img className={styles.linkicon} src={Linkedin} alt="Linkedin" />
+                    </a>
+                </li>
+                <li>
+                    <a className={styles.footer__link} href={'https://www.instagram.com/'} target="_blank" rel="noreferrer">
+                        <img className={styles.linkicon} src={Instagram} alt="Instagram" />
+                    </a>
+                </li>
+            </ul>
+            <p className={styles.footer__text}>
+                Made with 💗 on course{' '}
+                <a className={styles.footer__courselink} href="https://www.mastersacademy.education/frontend-for-beginners-it">
+                    `Intro to React` from Masters Academy in 2024
+                </a>{' '}
+                by Mariia
+            </p>
+        </div>
+    </footer>
+);
--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -1,2 +1,59 @@
+import carticon from '@/assets/cart.png';
+import loginicon from '@/assets/Login.png';
+import logo from '@/assets/logo.png';
+import mobileMenu from '@/assets/MobileMenu.png';
+import DarkThemeBtn from '@/assets/moon.png';
+import signicon from '@/assets/Sign.png';
+import LightThemeBtn from '@/assets/Sun.png';
+
 import styles from './header.module.css';
-export const HeaderComponent = () => <header className={styles.header}>Header</header>;
+
+export const HeaderComponent = () => (
+    <header className={styles.header}>
+        <div className={styles.wrapper}>
+            <img className={styles.logo} src={logo} alt="logo" />
+            <div className={styles.header__themes}>
+                <button className={styles.header__themebtn}>
+                    <img className={styles.header__themeimg} src={LightThemeBtn} alt="LightTheme" />
+                </button>
+
+                <button className={styles.header__themeBtn}>
+                    <img className={styles.header__themeimg} src={DarkThemeBtn} alt="DarkTheme" />
+                </button>
+            </div>
+
+            <nav className={styles.header__nav}>
+                <ul className={styles.header__navwrapper}>
+                    <li>
+                        <a className={styles.header__linkactive} href="/">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a className={styles.header__link} href="/">
+                            Products
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className={styles.header__navright}>
+                <button className={styles.header__cart}>
+                    <img className={styles.header__carticon} src={carticon} alt="cart" />
+                </button>
+                <button className={styles.header__mobilemenu}>
+                    <img className={styles.header__mobilemenu} src={mobileMenu} alt="Mobile Menu" />
+                </button>
+                <button className={styles.header__login}>
+                    <img className={styles.header__loginicon} src={loginicon} alt="Login" />
+                    Login
+                </button>
+
+                <button className={styles.header__sign} title="Sign">
+                    <img className={styles.header__signicon} src={signicon} alt="Sign" />
+                    Sign Up
+                </button>
+            </div>
+        </div>
+    </header>
+);
