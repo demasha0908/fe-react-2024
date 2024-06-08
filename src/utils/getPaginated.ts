3c2eb5af596be54aea0134068293466813b906f8
demasha0908 <demchenko.mariia0908@gmail.com>
2024-06-08 10:46:05 +0300

    fixed bug

--- src/utils/getPaginated.ts
+++
@@ -1,3 +0,0 @@
-import type { Product } from '@/interfaces/Product.ts';
-
-export const getPaginatedProducts = (data: Product[], currentPage: number): Product[] => data.slice((currentPage - 1) * 8, currentPage * 8);
